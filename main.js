39a0fc48e1fd6e91d81792cdcc68bb9e509e331b
giovdigua <giovdigua@gmail.com>
2019-11-27 18:22:48 +0100

    Final slide

---
+++ main.js
@@ -0,0 +1,66 @@
+$(document).ready(function() {
+    //Prec
+    $('#leftContainer').click(function() {
+
+        //Variables for images
+        var index = $('img');
+        var current = $('img.active');
+        var prec;
+        //Variables for the bullets
+        var bulletIndex = $('.bullet');
+        var currentBullet = $('.bullet.select');
+        var precBullet;
+
+        if (current.is(index.first())) {
+            prec = index.last();
+        }else{
+            prec = current.prev('img');
+
+        }
+
+        if (currentBullet.is(bulletIndex.first())) {
+            precBullet = bulletIndex.last();
+        } else {
+            precBullet = currentBullet.prev('.bullet');
+        }
+
+        current.removeClass('active');
+        prec.addClass('active');
+        currentBullet.removeClass('select');
+        precBullet.addClass('select');
+
+    });
+    //next
+    $('#rightContainer').click(function() {
+
+        //Variables for images
+        var index = $('img');
+        var current = $('img.active');
+        var next;
+        //Variables for the bullets
+        var bulletIndex = $('.bullet');
+        var currentBullet = $('.bullet.select');
+        var nextBullet;
+
+
+        if (current.is(index.last())) {
+            next = index.first();
+        }else{
+            next = current.next('img');
+
+        }
+
+        if (currentBullet.is(bulletIndex.last())) {
+            nextBullet = bulletIndex.first();
+        } else {
+            nextBullet = currentBullet.next('.bullet');
+        }
+
+        current.removeClass('active');
+        next.addClass('active');
+        currentBullet.removeClass('select');
+        nextBullet.addClass('select');
+
+    });
+
+});
